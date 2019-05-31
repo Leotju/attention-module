96f878fc4c3b0285be71acfeb78197d18456d0cc
Leotju <xj65112338>
2019-05-31 23:50:28 +0800

    0531

--- scripts/train_imagenet_resnet50_cbam.sh
+++ scripts/train_imagenet_resnet50_cbam.sh
@@ -1,9 +1,2 @@
-python train_imagenet.py \
-			--ngpu 8 \
-			--workers 20 \
-			--arch resnet --depth 50 \
-			--epochs 100 \
-			--batch-size 256 --lr 0.1 \
-			--att-type CBAM \
-			--prefix RESNET50_IMAGENET_CBAM \
-			./data/ImageNet/
+sleep 3h
+python train_imagenet.py --ngpu 2 --workers 8 --arch resnet --depth 18 --epochs 100 --batch-size 256 --lr 0.1 --att-type CBAM --prefix ../RESNET18_IMAGENET_CBAM /Datasets/imagenet/
