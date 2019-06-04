755f7d09bdb09d7a23a7cd3bbab66113f3406548
Leotju <xj65112338>
2019-06-04 21:16:28 +0800

    0531

--- scripts/train_imagenet_resnet18_cbam.sh
+++ scripts/train_imagenet_resnet18_cbam.sh
@@ -12,5 +12,5 @@ python train_imagenet.py \
 CUDA_VISIBLE_DEVICES=0,1,2,3 python train_imagenet.py --ngpu 4 --workers 16 --arch resnet --depth 18 --epochs 100 --batch-size 256 --lr 0.1 --att-type CBAM --prefix ../RESNET18_IMAGENET_CBAM /raid/tiancai/imagenet/imagenet/
 CUDA_VISIBLE_DEVICES=4,5,6,7 python train_imagenet.py --ngpu 4 --workers 16 --arch resnetbu --depth 18 --epochs 100 --batch-size 256 --lr 0.1 --att-type CBAM --prefix ../RESNET18BU_IMAGENET_CBAM /raid/tiancai/imagenet/imagenet/
 
-CUDA_VISIBLE_DEVICES=0,1 python train_imagenet.py --ngpu 4 --workers 8 --arch resnet_cascade --depth 18 --epochs 100 --batch-size 256 --lr 0.1 --att-type CBAM --prefix ../../chekpoint/RESNET18_IMAGENET_CAS_SE /Datasets/imagenet/
+CUDA_VISIBLE_DEVICES=0,1 python train_imagenet.py --ngpu 2 --workers 8 --arch resnet_cascade --depth 18 --epochs 100 --batch-size 256 --lr 0.1 --att-type CAS_SE --prefix ../../chekpoint/RESNET18_IMAGENET_CAS_SE /Datasets/imagenet/
 
